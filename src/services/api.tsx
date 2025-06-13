148d15c60c2b955fa15ea76180cd35d77e4658f9
Amit Mishra <65525769+DaSeeker67@users.noreply.github.com>
2025-06-14 00:23:57 +0530

    syncing to backend at render

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL:'http://localhost:4443'
+    baseURL:'https://the-zenchat-backend.onrender.com'
 })
 
 API.interceptors.request.use((req)=>{
